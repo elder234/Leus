19ae0a52de1330a54c46b8798484522489e48b4e
elder234 <97814196+elder234@users.noreply.github.com>
2024-07-13 10:31:29 +0000

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
 import asyncio
 import os
-from tzlocal import get_localzone
-from pytz import timezone
 from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from hydrogram import Client as tgClient, enums, filters
 from pymongo import MongoClient
-from asyncio import Lock
 from dotenv import load_dotenv, dotenv_values
-from threading import Thread
-from time import sleep, time
-from subprocess import Popen, run as srun
-from os import remove as osremove, path as ospath, environ, getcwd
+from logging import getLogger, Formatter, FileHandler, StreamHandler, INFO, error as log_error, info as log_info, warning as log_warning
+
+# Additional imports you may need
+from os import path as ospath
 from aria2p import API as ariaAPI, Client as ariaClient
 from qbittorrentapi import Client as qbClient
 from faulthandler import enable as faulthandler_enable
 from socket import setdefaulttimeout
-from logging import getLogger, Formatter, FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info, warning as log_warning
 from uvloop import install
 
 faulthandler_enable()
 install()
 setdefaulttimeout(600)
 
-botStartTime = time()
-
-basicConfig(format="[%(asctime)s] [%(levelname)s] - %(message)s", #  [%(filename)s:%(lineno)d]
+# Basic logging configuration
+basicConfig(format="[%(asctime)s] [%(levelname)s] - %(message)s",
             datefmt="%d-%b-%y %I:%M:%S %p",
             handlers=[FileHandler('log.txt'), StreamHandler()],
             level=INFO)
@@ -35,6 +30,7 @@ LOGGER = getLogger(__name__)
 
 load_dotenv('config.env', override=True)
 
+# Initialize variables and locks
 Interval = []
 QbInterval = []
 QbTorrents = {}
@@ -52,29 +48,24 @@ bot_cache = {}
 non_queued_dl = set()
 non_queued_up = set()
 
-try:
-    if bool(environ.get('_____REMOVE_THIS_LINE_____')):
-        log_error('The README.md file there to be read! Exiting now!')
-        exit()
-except:
-    pass
-
-download_dict_lock = Lock()
-status_reply_dict_lock = Lock()
-queue_dict_lock = Lock()
-qb_listener_lock = Lock()
+# Locks
+download_dict_lock = asyncio.Lock()
+status_reply_dict_lock = asyncio.Lock()
+queue_dict_lock = asyncio.Lock()
+qb_listener_lock = asyncio.Lock()
 status_reply_dict = {}
 download_dict = {}
 rss_dict = {}
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+# Fetching environment variables
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = BOT_TOKEN.split(':', 1)[0]
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = ''
 
@@ -95,7 +86,7 @@ async def initialize_db():
         elif config_dict := db.settings.config.find_one({'_id': bot_id}):
             del config_dict['_id']
             for key, value in config_dict.items():
-                environ[key] = str(value)
+                os.environ[key] = str(value)
         if pf_dict := db.settings.files.find_one({'_id': bot_id}):
             del pf_dict['_id']
             for key, value in pf_dict.items():
@@ -105,289 +96,26 @@ async def initialize_db():
                         f.write(value)
         if a2c_options := db.settings.aria2c.find_one({'_id': bot_id}):
             del a2c_options['_id']
-            aria2_options = a2c_options
+            aria2_options.update(a2c_options)  # Update instead of reassignment
         if qbit_opt := db.settings.qbittorrent.find_one({'_id': bot_id}):
             del qbit_opt['_id']
-            qbit_options = qbit_opt
+            qbit_options.update(qbit_opt)  # Update instead of reassignment
         conn.close()
-
-await initialize_db()
-
-OWNER_ID = environ.get('OWNER_ID', '')
-if len(OWNER_ID) == 0:
-    log_error("OWNER_ID variable is missing! Exiting now")
-    exit(1)
-else:
-    OWNER_ID = int(OWNER_ID)
-
-TELEGRAM_API = environ.get('TELEGRAM_API', '')
-if len(TELEGRAM_API) == 0:
-    log_error("TELEGRAM_API variable is missing! Exiting now")
-    exit(1)
-else:
-    TELEGRAM_API = int(TELEGRAM_API)
-
-TELEGRAM_HASH = environ.get('TELEGRAM_HASH', '')
-if len(TELEGRAM_HASH) == 0:
-    log_error("TELEGRAM_HASH variable is missing! Exiting now")
-    exit(1)
-
-TIMEZONE = environ.get('TIMEZONE', '')
-if len(TIMEZONE) == 0:
-    TIMEZONE = 'Asia/Kolkata'
-
-def changetz(*args):
-    return datetime.now(timezone(TIMEZONE)).timetuple()
-Formatter.converter = changetz
-log_info("TIMEZONE synced with logging status")
-
-GDRIVE_ID = environ.get('GDRIVE_ID', '')
-if len(GDRIVE_ID) == 0:
-    GDRIVE_ID = ''
-
-RCLONE_PATH = environ.get('RCLONE_PATH', '')
-if len(RCLONE_PATH) == 0:
-    RCLONE_PATH = ''
-
-RCLONE_FLAGS = environ.get('RCLONE_FLAGS', '')
-if len(RCLONE_FLAGS) == 0:
-    RCLONE_FLAGS = ''
-
-DEFAULT_UPLOAD = environ.get('DEFAULT_UPLOAD', '')
-if DEFAULT_UPLOAD != 'rc' and DEFAULT_UPLOAD != 'ddl':
-    DEFAULT_UPLOAD = 'gd'
-
-DOWNLOAD_DIR = environ.get('DOWNLOAD_DIR', '')
-if len(DOWNLOAD_DIR) == 0:
-    DOWNLOAD_DIR = '/usr/src/app/downloads/'
-elif not DOWNLOAD_DIR.endswith("/"):
-    DOWNLOAD_DIR = f'{DOWNLOAD_DIR}/'
-
-AUTHORIZED_CHATS = environ.get('AUTHORIZED_CHATS', '')
-if AUTHORIZED_CHATS:
-    aid = AUTHORIZED_CHATS.split()
-    for id_ in aid:
-        chat_id, *topic_ids = id_.split(':')
-        chat_id = int(chat_id)
-        user_data.setdefault(chat_id, {'is_auth': True})
-        if topic_ids:
-            user_data[chat_id].setdefault('topic_ids', []).extend(map(int, topic_ids))
-
-SUDO_USERS = environ.get('SUDO_USERS', '')
-if len(SUDO_USERS) != 0:
-    aid = SUDO_USERS.split()
-    for id_ in aid:
-        user_data[int(id_.strip())] = {'is_sudo': True}
-
-BLACKLIST_USERS = environ.get('BLACKLIST_USERS', '')
-if len(BLACKLIST_USERS) != 0:
-    for id_ in BLACKLIST_USERS.split():
-        user_data[int(id_.strip())] = {'is_blacklist': True}
-
-EXTENSION_FILTER = environ.get('EXTENSION_FILTER', '')
-if len(EXTENSION_FILTER) > 0:
-    fx = EXTENSION_FILTER.split()
-    for x in fx:
-        x = x.lstrip('.')
-        GLOBAL_EXTENSION_FILTER.append(x.strip().lower())
-
-LINKS_LOG_ID = environ.get('LINKS_LOG_ID', '')
-LINKS_LOG_ID = '' if len(LINKS_LOG_ID) == 0 else int(LINKS_LOG_ID)
-
-MIRROR_LOG_ID = environ.get('MIRROR_LOG_ID', '')
-if len(MIRROR_LOG_ID) == 0:
-    MIRROR_LOG_ID = ''
-
-LEECH_LOG_ID = environ.get('LEECH_LOG_ID', '')
-if len(LEECH_LOG_ID) == 0:
-    LEECH_LOG_ID = ''
-
-EXCEP_CHATS = environ.get('EXCEP_CHATS', '')
-if len(EXCEP_CHATS) == 0:
-    EXCEP_CHATS = ''
-
-IS_PREMIUM_USER = False
-user = ''
-USER_SESSION_STRING = environ.get('USER_SESSION_STRING', '')
-if len(USER_SESSION_STRING) != 0:
-    log_info("Creating client from USER_SESSION_STRING")
-    try:
-        user = await tgClient('user', TELEGRAM_API, TELEGRAM_HASH, session_string=USER_SESSION_STRING,
-                        parse_mode=enums.ParseMode.HTML, no_updates=True, max_concurrent_transmissions=1000).start()
-        IS_PREMIUM_USER = user.me.is_premium
-    except Exception as e:
-        log_error(f"Failed making client from USER_SESSION_STRING : {e}")
-        user = ''
-
-MEGA_EMAIL = environ.get('MEGA_EMAIL', '')
-MEGA_PASSWORD = environ.get('MEGA_PASSWORD', '')
-if len(MEGA_EMAIL) == 0 or len(MEGA_PASSWORD) == 0:
-    log_warning('MEGA Credentials not provided!')
-    MEGA_EMAIL = ''
-    MEGA_PASSWORD = ''
-
-GDTOT_CRYPT = environ.get('GDTOT_CRYPT', '')
-if len(GDTOT_CRYPT) == 0:
-    GDTOT_CRYPT = ''
-
-JIODRIVE_TOKEN = environ.get('JIODRIVE_TOKEN', '')
-if len(JIODRIVE_TOKEN) == 0:
-    JIODRIVE_TOKEN = ''
-
-HUBDRIVE_CRYPT = environ.get('HUBDRIVE_CRYPT', '')
-if len(HUBDRIVE_CRYPT) == 0:
-    HUBDRIVE_CRYPT = ''
-
-KOLOP_CRYPT = environ.get('KOLOP_CRYPT', '')
-if len(KOLOP_CRYPT) == 0:
-    KOLOP_CRYPT = ''
-
-SHARERPW_CRYPT = environ.get('SHARERPW_CRYPT', '')
-if len(SHARERPW_CRYPT) == 0:
-    SHARERPW_CRYPT = ''
-
-HUBDRIVE_EMAIL = environ.get('HUBDRIVE_EMAIL', '')
-HUBDRIVE_PASS = environ.get('HUBDRIVE_PASS', '')
-if len(HUBDRIVE_EMAIL) == 0 or len(HUBDRIVE_PASS) == 0:
-    HUBDRIVE_EMAIL = ''
-    HUBDRIVE_PASS = ''
-
-TORRENT_TIMEOUT = environ.get('TORRENT_TIMEOUT', '')
-if len(TORRENT_TIMEOUT) == 0:
-    TORRENT_TIMEOUT = 600
-else:
-    TORRENT_TIMEOUT = int(TORRENT_TIMEOUT)
-
-QUEUE_ALL = environ.get('QUEUE_ALL', '')
-if len(QUEUE_ALL) == 0:
-    QUEUE_ALL = False
-else:
-    QUEUE_ALL = True
-
-INCOMPLETE_TASK_NOTIFIER = environ.get('INCOMPLETE_TASK_NOTIFIER', '')
-if len(INCOMPLETE_TASK_NOTIFIER) == 0:
-    INCOMPLETE_TASK_NOTIFIER = False
-else:
-    INCOMPLETE_TASK_NOTIFIER = True
-
-BASE_URL = environ.get('BASE_URL', '').rstrip("/")
-if len(BASE_URL) == 0:
-    BASE_URL = ''
-
-WEB_PINCODE = environ.get('WEB_PINCODE', '')
-if len(WEB_PINCODE) == 0:
-    WEB_PINCODE = False
-else:
-    WEB_PINCODE = True
-
-BOT_MAX_TASKS = environ.get('BOT_MAX_TASKS', '')
-if len(BOT_MAX_TASKS) == 0:
-    BOT_MAX_TASKS = False
-else:
-    BOT_MAX_TASKS = int(BOT_MAX_TASKS)
-
-SHOW_LIMITS = environ.get('SHOW_LIMITS', '')
-if len(SHOW_LIMITS) == 0:
-    SHOW_LIMITS = False
-else:
-    SHOW_LIMITS = True
-
-DAILY_TASK_LIMIT = environ.get('DAILY_TASK_LIMIT', '')
-if len(DAILY_TASK_LIMIT) == 0:
-    DAILY_TASK_LIMIT = 0
-else:
-    DAILY_TASK_LIMIT = int(DAILY_TASK_LIMIT)
-
-DAILY_MIRROR_LIMIT = environ.get('DAILY_MIRROR_LIMIT', '')
-if len(DAILY_MIRROR_LIMIT) == 0:
-    DAILY_MIRROR_LIMIT = 0
-else:
-    DAILY_MIRROR_LIMIT = int(DAILY_MIRROR_LIMIT)
-
-DAILY_LEECH_LIMIT = environ.get('DAILY_LEECH_LIMIT', '')
-if len(DAILY_LEECH_LIMIT) == 0:
-    DAILY_LEECH_LIMIT = 0
-else:
-    DAILY_LEECH_LIMIT = int(DAILY_LEECH_LIMIT)
-
-STORAGE_THRESHOLD = environ.get('STORAGE_THRESHOLD', '')
-if len(STORAGE_THRESHOLD) == 0:
-    STORAGE_THRESHOLD = 0
-else:
-    STORAGE_THRESHOLD = int(STORAGE_THRESHOLD)
-
-RCLONE_SERVE_URL = environ.get('RCLONE_SERVE_URL', '')
-if len(RCLONE_SERVE_URL) == 0:
-    RCLONE_SERVE_URL = ''
-
-LEECH_SPLIT_SIZE = environ.get('LEECH_SPLIT_SIZE', '')
-if len(LEECH_SPLIT_SIZE) == 0:
-    LEECH_SPLIT_SIZE = 2097152000
-else:
-    LEECH_SPLIT_SIZE = int(LEECH_SPLIT_SIZE)
-
-ARIA_PORT = environ.get('ARIA_PORT', '')
-if len(ARIA_PORT) == 0:
-    ARIA_PORT = 6800
-else:
-    ARIA_PORT = int(ARIA_PORT)
-
-qb_client = qbClient(host='localhost', port=8090,
-                    username=environ.get('QBITTORRENT_USERNAME', ''),
-                    password=environ.get('QBITTORRENT_PASSWORD', ''))
-
-aria2 = ariaAPI(ariaClient(
-    host='http://localhost', port=ARIA_PORT, secret=''))
-
-DOWNLOAD_STATUS_UPDATE_INTERVAL = 5
-
-if not ospath.exists(DOWNLOAD_DIR):
-    os.makedirs(DOWNLOAD_DIR)
-
-async def add_aria2c_torrent(torrent_url, download_path):
-    options = aria2_options.copy()
-    if download_path:
-        options['dir'] = download_path
-    torrent_download = await aria2.add_torrent(torrent_url, options=options)
-    return torrent_download
-
-async def add_qbit_torrent(torrent_url, download_path):
-    options = qbit_options.copy()
-    if download_path:
-        options['savepath'] = download_path
-    torrent_download = await qb_client.torrents_add(torrent_files=torrent_url, save_path=download_path)
-    return torrent_download
-
-bot = tgClient("bot", bot_token=BOT_TOKEN, api_id=TELEGRAM_API, api_hash=TELEGRAM_HASH)
-
-@bot.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-    await message.reply("Hello! I am alive.")
-
-@bot.on_message(filters.command("help") & filters.private)
-async def help(client, message):
-    await message.reply("Here is the help text.")
-
-@bot.on_message(filters.command("add_torrent") & filters.private)
-async def add_torrent(client, message):
-    if len(message.command) != 2:
-        await message.reply("Usage: /add_torrent <torrent_url>")
-        return
-    torrent_url = message.command[1]
-    await add_aria2c_torrent(torrent_url, DOWNLOAD_DIR)
-    await message.reply("Torrent added!")
 
 async def main():
     scheduler = AsyncIOScheduler()
+
+    # Initialize database asynchronously
     await initialize_db()
 
     # Scheduled jobs
-    scheduler.add_job(check_download_status, 'interval', seconds=DOWNLOAD_STATUS_UPDATE_INTERVAL)
+    scheduler.add_job(check_download_status, 'interval', seconds=5)
     scheduler.add_job(refresh_torrent_info, 'interval', minutes=15)
     scheduler.add_job(cleanup_old_tasks, 'interval', hours=1)
 
     scheduler.start()
     log_info("Bot Started!")
+
     try:
         await bot.start()
         await bot.idle()
@@ -398,15 +126,15 @@ async def main():
         log_info("Bot stopped!")
 
 async def check_download_status():
-    # Code to check and update the download status
+    # Placeholder for download status checking logic
     pass
 
 async def refresh_torrent_info():
-    # Code to refresh torrent information
+    # Placeholder for torrent information refreshing logic
     pass
 
 async def cleanup_old_tasks():
-    # Code to clean up old tasks
+    # Placeholder for old task cleanup logic
     pass
 
 if __name__ == "__main__":
